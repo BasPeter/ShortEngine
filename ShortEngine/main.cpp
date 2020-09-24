0449c87d5b5c760a7c7b1f3a743ce2410dc07a54
BasPeter <baspeterdijkema@hotmail.com>
2020-09-24 16:29:49 +0200

    Initial Commit

---
+++ ShortEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ShortEngine
+//
+//  Created by Bas Peter Dijkema on 24/09/2020.
+//  Copyright © 2020 Bas Peter Dijkema. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
